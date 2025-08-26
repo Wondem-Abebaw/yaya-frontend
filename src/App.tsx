96c06d05584505ab29382649c8f6a832a0e6cdc1
Wondem Abebaw <wondem5060@gmail.com>
2025-08-26 22:36:03 +0300

    build error fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import TransactionDashboard from "./components/TransactionDashboard";
 import "./App.css";
 
--- src/hooks/useTransactions.ts
+++ src/hooks/useTransactions.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useCallback } from "react";
 import { Transaction, PaginatedResponse } from "../types/transaction";
 import { apiClient } from "../utils/api";
 
